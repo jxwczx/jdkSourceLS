55d94c26534718b26c0a12aef4f42310ba878aa5
JXWCZX <2424749442@qq.com>
2021-09-26 21:48:47 +0800

    提交新项目

---
+++ src/main/java/com/jc/jdktest/JdktestApplication.java
@@ -0,0 +1,13 @@
+package com.jc.jdktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdktestApplication.class, args);
+    }
+
+}
